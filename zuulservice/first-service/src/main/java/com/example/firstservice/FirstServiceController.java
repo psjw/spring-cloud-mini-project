eb7afd7904dbdbac9d4e3d24e6341cfa5404bfd1
psjw <dlrudtn108@gmail.com>
2022-02-06 16:08:13 +0900

    spring cloud yml -> javaconfig

--- zuulservice/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ zuulservice/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,11 +1,14 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // http://localhost:8081/welcome
 // http://localhost:8081/first-service/welcome
+@Slf4j
 @RestController
 //@RequestMapping("/")//Zuul Gateway API
 @RequestMapping("/first-service")//Spring Gateway API
@@ -13,5 +16,12 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service.";
+    }
+
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello World in First Service.";
     }
 }
